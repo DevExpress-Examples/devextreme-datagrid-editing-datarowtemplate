cb82f693619a72facaaa330946ab0f45ac0f9b8e
Moldir Zholymbetova <moldir.zholymbetova@devexpress.com>
2024-03-15 17:12:30 +0500

    asp example 2

--- ASP.NET Core/Controllers/HomeController.cs
+++ ASP.NET Core/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NET_Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Core.Controllers
@@ -10,7 +11,8 @@ namespace ASP_NET_Core.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var editors = new List<string> { "Prefix", "FirstName", "LastName", "Position", "BirthDate", "HireDate" };
+            return View(editors);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
