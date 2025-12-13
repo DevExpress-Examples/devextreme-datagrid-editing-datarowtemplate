bae7d77f97c6706804caeadc30cf392e354c9833
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-12-13 10:54:28 +0200

    Migrate to eslint-config-devextreme 1.1.8

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import DataGrid, { DataChange, InitNewRowEvent, InitializedEvent } from 'devextreme/ui/data_grid';
-import { AppService, Employee, ValueChanged } from './app.service';
+import { ClickEvent } from 'devextreme/ui/button';
 
 @Component({
   selector: 'app-root',
@@ -8,72 +7,14 @@ import { AppService, Employee, ValueChanged } from './app.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  dataGrid?: DataGrid | undefined;
+  title = 'Angular';
 
-  employees: Employee[];
+  counter = 0;
 
-  changes: DataChange[];
+  buttonText = 'Click count: 0';
 
-  editors: string[];
-
-  constructor(service: AppService) {
-    this.employees = service.getEmployees();
-    this.changes = [];
-    this.editors = ['Prefix', 'FirstName', 'LastName', 'Position', 'BirthDate', 'HireDate'];
+  onClick(_e: ClickEvent): void {
+    this.counter++;
+    this.buttonText = `Click count: ${this.counter}`;
   }
-
-  saveGridInstance(e: InitializedEvent): void {
-    this.dataGrid = e.component;
-  }
-
-  onInitNewRow = (e: InitNewRowEvent): void => {
-    e.data.Prefix = '';
-    e.data.FirstName = '';
-    e.data.LastName = '';
-    e.data.Position = '';
-    e.data.BirthDate = new Date('1986/07/08');
-    e.data.HireDate = new Date('1986/07/08');
-    e.data.Notes = '';
-  };
-
-  addNewItem = (): void => {
-    this.dataGrid?.cancelEditData();
-    this.dataGrid?.addRow().then(
-      () => this.changes = this.dataGrid?.option('editing.changes') as DataChange[],
-      () => {},
-    );
-  };
-
-  onValueChanged = (val: ValueChanged): void => {
-    if (!this.changes.length) {
-      this.changes.push({ data: { [val.dataField]: val.e.value }, key: val.key, type: 'update' });
-    } else {
-      this.changes[0].data = { ...this.changes[0].data, [val.dataField]: val.e.value };
-    }
-  };
-
-  onEditButtonClick = (ID: number): void => {
-    const rowIndex = this.dataGrid?.getRowIndexByKey(ID);
-    this.dataGrid?.cancelEditData();
-    if (rowIndex != null) this.dataGrid?.editRow(rowIndex);
-    this.changes = this.dataGrid?.option('editing.changes') as DataChange[];
-  };
-
-  onDeleteButtonClick = (ID: number): void => {
-    const rowIndex = this.dataGrid?.getRowIndexByKey(ID);
-    if (rowIndex != null) this.dataGrid?.deleteRow(rowIndex);
-  };
-
-  onSaveButtonClick = (): void => {
-    this.dataGrid?.option('editing.changes', this.changes);
-    this.dataGrid?.saveEditData().then(
-      () => this.dataGrid?.refresh(),
-      () => {},
-    );
-  };
-
-  onCancelButtonClick = (): void => {
-    this.dataGrid?.cancelEditData();
-    this.dataGrid?.refresh().catch(() => {});
-  };
 }
